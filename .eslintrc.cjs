1cca30d47945ad2a7fbd4760b0f8d16ceb27d642
kapureka6174 <mitsu.n.0109@gmail.com>
2023-02-06 23:27:37 +0900

    chore: add ESLint, Prettier, lint-staged to automate static analysis of code

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:react/jsx-runtime',
+    'prettier'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: {},
+  settings: {
+    react: {
+      version: '18.2.0'
+    }
+  }
+}
